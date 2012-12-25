4fe2a2af1044b96f8f37c97b806af82567fce087
Anthony Ticknor <tony.ticknor@gmail.com>
2012-12-24 22:28:56 -0600

    adding wrapper around all of the markup

--- index.php
+++ index.php
@@ -2,107 +2,113 @@
 <?php require_once('includes/functions.php'); ?>
 <?php require_once('includes/head.php'); ?>
 
-        <!--- SECTION 1 -->
-        <div class="section" role="banner">
-            <h1>Straight Line Theory</h1>
-            <h2>Straight Line Theory is a team of information architects and usability experts based in Minneapolis.</h2>
-            <a class="btn" href="#contact">Contact Us</a>
-        </div>
-        <!-- // END SECTION 1 -->
+        <!-- WRAPPER -->
+        <div class="wrapper">
 
-        <!--- SECTION 2 -->
-        <div class="section">
-            <h2>We help companies simplify and improve their websites, applications and mobile experiences.</h2>
-            <ul>
-                <li>
-                    <h3>Understand Your Users.</h3>
-                    <p>Do you have a complete picture of the people you want to reach? Through user research and ethnographic studies, we'll help you gain a deeper understanding of your users' behaviors, motivations and goals.</p>
-                </li>
-                <li>
-                    <h3>Define the Experience.</h3>
-                    <p>We're experts at creating experiences that are easy to use. We'll work with you to plan and prototype the right solution. Our sitemaps, task models and wireframes capture all the details and help you visualize every interaction.</p>
-                </li>
-                <li>
-                    <h3>Test, Evaluate and Improve.</h3>
-                    <p>We'll test your site or product with real people to see how well it supports their needs. Then we'll provide straightforward solutions for fixing any problems. No long-winded reports, just the stuff you need to make it better.</p>
-                </li>
-            </ul>
-        </div>
-        <!-- // END SECTION 2 -->
-
-        <!--- SECTION 3 -->
-        <div class="section">
-            <h2>We're passionate about solving complex user experience challenges.</h2>
-        </div>
-        <!-- // END SECTION 3 -->
-
-        <!--- SECTION 4 -->
-        <div class="section">
-            <h2>Meet Straight Line Theory</h2>
-            <p>We're a highly-skilled team of experienced UX professionals. We trill the challenge of turning complex business problems into simple, elegant solutions. We love to sketch, protoype, and explore what's possible. Whether your goal is to bvoost sales or improve customer satisfaction. We can help.</p>
-            <p>Interested in joining our Team? <a href="mailto:jobs@straightlinetheory.com">jobs@straightlinetheory.com</a></p>
-            <ul>
-                <li>
-                    <h3>Andrew Benson</h3>
-                </li>
-                <li>
-                    <h3>Kristen Skifstad</h3>
-                </li>
-                <li>
-                    <h3>Jay Mathews</h3>
-                </li>
-                <li>
-                    <h3>Laura Horan</h3>
-                </li>
-                <li>
-                    <h3>Roy Neal</h3>
-                </li>
-                <li>
-                    <h3>John Dusek</h3>
-                </li>
-            </ul>
-            <h2>How We're Different</h2>
-            <ul>
-                <li>
-                    <h3>Understand Your Users</h3>
-                    <p>We started Straight Line Theory in 2002 to focus exclusively on creating great digital experiences that are easy to use. Doing one thing keeps us lean and sharp.</p>
-                </li>
-                <li>
-                    <h3>We Understand Business</h3>
-                    <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
-                </li>
-                <li>
-                    <h3>We're small but get big results</h3>
-                    <p>Being small means you'll work directly with talented information architects and usability experts who are passionate about what we do and invested in your success.</p>
-                </li>
-                <li>
-                    <h3>We cut out the unimportant stuff</h3>
-                    <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
-                </li>
-            </ul>
-        </div>
-        <!-- // END SECTION 4 -->
-
-        <!--- SECTION 5 -->
-        <div class="section" role="contentinfo">
-            <h2>Want to know more? Contact Us.</h2>
-            <div class="vcard">
-                <h3 class="fn org">Straight Line Theory</h3>
-                <div class="adr">
-                    <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
-                    <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span><br/>
-                    <a class="email" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
-                    <span class="tel">
-                        <span class="type isHidden">work</span>
-                        <span class="value">(612) 455-3388</span>
-                    </span>
-                    <span class="tel">
-                        <span class="type isHidden">fax</span>
-                        <span class="value">(612) 455-3389</span>
-                    </span>
+            <!--- SECTION 1 -->
+            <div class="section" role="banner">
+                <h1>Straight Line Theory</h1>
+                <h2>Straight Line Theory is a team of information architects and usability experts based in Minneapolis.</h2>
+                <a class="btn" href="#contact">Contact Us</a>
+            </div>
+            <!-- // END SECTION 1 -->
+    
+            <!--- SECTION 2 -->
+            <div class="section">
+                <h2>We help companies simplify and improve their websites, applications and mobile experiences.</h2>
+                <ul>
+                    <li>
+                        <h3>Understand Your Users.</h3>
+                        <p>Do you have a complete picture of the people you want to reach? Through user research and ethnographic studies, we'll help you gain a deeper understanding of your users' behaviors, motivations and goals.</p>
+                    </li>
+                    <li>
+                        <h3>Define the Experience.</h3>
+                        <p>We're experts at creating experiences that are easy to use. We'll work with you to plan and prototype the right solution. Our sitemaps, task models and wireframes capture all the details and help you visualize every interaction.</p>
+                    </li>
+                    <li>
+                        <h3>Test, Evaluate and Improve.</h3>
+                        <p>We'll test your site or product with real people to see how well it supports their needs. Then we'll provide straightforward solutions for fixing any problems. No long-winded reports, just the stuff you need to make it better.</p>
+                    </li>
+                </ul>
+            </div>
+            <!-- // END SECTION 2 -->
+    
+            <!--- SECTION 3 -->
+            <div class="section">
+                <h2>We're passionate about solving complex user experience challenges.</h2>
+            </div>
+            <!-- // END SECTION 3 -->
+    
+            <!--- SECTION 4 -->
+            <div class="section">
+                <h2>Meet Straight Line Theory</h2>
+                <p>We're a highly-skilled team of experienced UX professionals. We trill the challenge of turning complex business problems into simple, elegant solutions. We love to sketch, protoype, and explore what's possible. Whether your goal is to bvoost sales or improve customer satisfaction. We can help.</p>
+                <p>Interested in joining our Team? <a href="mailto:jobs@straightlinetheory.com">jobs@straightlinetheory.com</a></p>
+                <ul>
+                    <li>
+                        <h3>Andrew Benson</h3>
+                    </li>
+                    <li>
+                        <h3>Kristen Skifstad</h3>
+                    </li>
+                    <li>
+                        <h3>Jay Mathews</h3>
+                    </li>
+                    <li>
+                        <h3>Laura Horan</h3>
+                    </li>
+                    <li>
+                        <h3>Roy Neal</h3>
+                    </li>
+                    <li>
+                        <h3>John Dusek</h3>
+                    </li>
+                </ul>
+                <h2>How We're Different</h2>
+                <ul>
+                    <li>
+                        <h3>Understand Your Users</h3>
+                        <p>We started Straight Line Theory in 2002 to focus exclusively on creating great digital experiences that are easy to use. Doing one thing keeps us lean and sharp.</p>
+                    </li>
+                    <li>
+                        <h3>We Understand Business</h3>
+                        <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
+                    </li>
+                    <li>
+                        <h3>We're small but get big results</h3>
+                        <p>Being small means you'll work directly with talented information architects and usability experts who are passionate about what we do and invested in your success.</p>
+                    </li>
+                    <li>
+                        <h3>We cut out the unimportant stuff</h3>
+                        <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
+                    </li>
+                </ul>
+            </div>
+            <!-- // END SECTION 4 -->
+    
+            <!--- SECTION 5 -->
+            <div class="section" role="contentinfo">
+                <h2>Want to know more? Contact Us.</h2>
+                <div class="vcard">
+                    <h3 class="fn org">Straight Line Theory</h3>
+                    <div class="adr">
+                        <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
+                        <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span><br/>
+                        <a class="email" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
+                        <span class="tel">
+                            <span class="type isHidden">work</span>
+                            <span class="value">(612) 455-3388</span>
+                        </span>
+                        <span class="tel">
+                            <span class="type isHidden">fax</span>
+                            <span class="value">(612) 455-3389</span>
+                        </span>
+                    </div>
                 </div>
             </div>
+            <!-- // END SECTION 5 -->
+
         </div>
-        <!-- // END SECTION 5 -->
+        <!-- // END WRAPPER -->
 
 <?php require_once('includes/foot.php'); ?>
