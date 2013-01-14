20417986fa8640125f335890b9aa79484d605ef7
Anthony Ticknor <tony.ticknor@gmail.com>
2013-01-13 21:16:10 -0600

    addplying hdg subsection styles to services section

--- includes/sections/services.php
+++ includes/sections/services.php
@@ -5,19 +5,19 @@
                 <ul class="blocks blocks_3up">
                     <li>
                         <div class="box">
-                            <h3>Understand Your Users.</h3>
+                            <h3 class="hdg_subSection hdg_green">Understand Your Users.</h3>
                             <p>Do you have a complete picture of the people you want to reach? Through user research and ethnographic studies, we'll help you gain a deeper understanding of your users' behaviors, motivations and goals.</p>
                         </div>
                     </li>
                     <li>
                         <div class="box">
-                            <h3>Define the Experience.</h3>
+                            <h3 class="hdg_subSection hdg_green">Define the Experience.</h3>
                             <p>We're experts at creating experiences that are easy to use. We'll work with you to plan and prototype the right solution. Our sitemaps, task models and wireframes capture all the details and help you visualize every interaction.</p>
                         </div>
                     </li>
                     <li>
                         <div class="box">
-                            <h3>Test, Evaluate and Improve.</h3>
+                            <h3 class="hdg_subSection hdg_green">Test, Evaluate and Improve.</h3>
                             <p>We'll test your site or product with real people to see how well it supports their needs. Then we'll provide straightforward solutions for fixing any problems. No long-winded reports, just the stuff you need to make it better.</p>
                         </div>
                     </li>
