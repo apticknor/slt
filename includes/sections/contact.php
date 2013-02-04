74c0d9f410dad94e8227af45f5d3238d3810713c
Anthony Ticknor <tony.ticknor@gmail.com>
2013-02-04 12:04:33 -0600

    focusing map area to pin with a more fine grained breakdown

--- includes/sections/contact.php
+++ includes/sections/contact.php
@@ -1,29 +1,36 @@
         <!--- SECTION CONTACT -->
         <div class="section section_contact" role="contentinfo" id="page-contact">
             <div class="section-bd section-bd_contact">
-                <a href="http://goo.gl/maps/zxYDZ" rel="external" class="mapPin"></a>
-                <div class="mapOverlay">
-                    <div class="box box_mute">
-                        <div class="teaser">
-                            <div class="teaser-hd">
-                                <h2>Want to know more? Contact Us.</h2>
-                            </div>
-                            <div class="teaser-bd">
-                                <div class="vcard">
-                                    <h3 class="fn org">Straight Line Theory</h3>
-                                    <div class="adr">
-                                        <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
-                                        <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span><br/>
-                                        <a class="email" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
-                                        <span class="tel">
-                                            <span class="type isHidden">work</span>
-                                            <span class="value">(612) 455-3388</span>
-                                        </span>
-                                        <span class="tel">
-                                            <span class="type isHidden">fax</span>
-                                            <span class="value">(612) 455-3389</span>
-                                        </span>
+                <div class="map">
+                    <a href="http://goo.gl/maps/zxYDZ" rel="external" class="map-pin"></a>
+                    <div class="map-overlay">
+                        <div class="box box_mute box_basic">
+                            <div class="teaser">
+                                <div class="teaser-hd">
+                                    <h2 class="hdg hdg_subSection hdg_bold hdg_lightBlue">Want to know more? Contact Us.</h2>
+                                </div>
+                                <div class="teaser-subHd">
+                                    <h3 class="hdg hdg_minorSection hdg_bold hdg_cap"><span class="fn org">Straight Line Theory</span></h3>
+                                </div>
+                                <div class="teaser-bd">
+                                    <div class="vcard">
+                                        <div class="adr">
+                                            <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
+                                            <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span><br/>
+                                            <a class="email" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
+                                            <span class="tel">
+                                                <span class="type isHidden">work</span>
+                                                <span class="value">(612) 455-3388</span>
+                                            </span>
+                                            <span class="tel">
+                                                <span class="type isHidden">fax</span>
+                                                <span class="value">(612) 455-3389</span>
+                                            </span>
+                                        </div>
                                     </div>
+                                </div>
+                                <div class="teaser-ft">
+                                    <h3 class="hdg hdg_subSection hdg_bold hdg_lightGreen"><a href="http://goo.gl/maps/zxYDZ">Get Directions</a></h3>
                                 </div>
                             </div>
                         </div>
