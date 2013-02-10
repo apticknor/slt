8f45119cb6f081a98eb8460a8ee86bb8f0206aa5
Anthony Ticknor <tony.ticknor@gmail.com>
2013-02-10 12:43:41 -0600

    updates to about section

--- includes/sections/about.php
+++ includes/sections/about.php
@@ -4,6 +4,7 @@
                 <h2 class="hdg hdg_section hdg_blue">Meet Straight Line Theory</h2>
             </div>
             <div class="section-bd">
+                <h3 class="isHidden">Our Team</h3>
                 <p>We're a highly-skilled team of experienced UX professionals. We trill the challenge of turning complex business problems into simple, elegant solutions. We love to sketch, protoype, and explore what's possible. Whether your goal is to bvoost sales or improve customer satisfaction. We can help.</p>
                 <p>Interested in joining our Team? <a href="mailto:jobs@straightlinetheory.com">jobs@straightlinetheory.com</a></p>
                 <ul class="blocks blocks_2upThen3up">
@@ -13,7 +14,7 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/andrew-benson.jpg" alt="Andrew Benson" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">Andrew Benson</h3>
+                                <h4 class="hdg hdg_bold">Andrew Benson</h4>
                             </div>
                         </div>
                     </li>
@@ -23,7 +24,7 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/kristen-skifstad.jpg" alt="Kristen Skifstad" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">Kristen Skifstad</h3>
+                                <h4 class="hdg hdg_bold">Kristen Skifstad</h4>
                             </div>
                         </div>
                     </li>
@@ -33,7 +34,7 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/jay-mathews.jpg" alt="Jay Mathews" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">Jay Mathews</h3>
+                                <h4 class="hdg hdg_bold">Jay Mathews</h4>
                             </div>
                         </div>
                     </li>
@@ -43,7 +44,7 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/laura-horan.jpg" alt="Laura Horan" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">Laura Horan</h3>
+                                <h4 class="hdg hdg_bold">Laura Horan</h4>
                             </div>
                         </div>
                     </li>
@@ -53,7 +54,7 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/roy-neal.jpg" alt="Roy Neal" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">Roy Neal</h3>
+                                <h4 class="hdg hdg_bold">Roy Neal</h4>
                             </div>
                         </div>
                     </li>
@@ -63,27 +64,32 @@
                                 <img src="<?php echo $site_url; ?>assets/images/content/john-dusek.jpg" alt="John Dusek" />
                             </div>
                             <div class="feature-hd">
-                                <h3 class="hdg hdg_bold">John Dusek</h3>
+                                <h4 class="hdg hdg_bold">John Dusek</h4>
                             </div>
                         </div>
                     </li>
                 </ul>
+            </div>
+            <div class="section-hd section-hd_services">
                 <h2 class="hdg hdg_section hdg_blue">How We're Different</h2>
-                <ul>
+            </div>
+            <div class="section-bd">
+                <h3 class="isHidden">Our Difference</h3>
+                <ul class="blocks blocks_4up">
                     <li>
-                        <h3>Understand Your Users</h3>
+                        <h4 class="hdg hdg_white">Understand Your Users</h4>
                         <p>We started Straight Line Theory in 2002 to focus exclusively on creating great digital experiences that are easy to use. Doing one thing keeps us lean and sharp.</p>
                     </li>
                     <li>
-                        <h3>We Understand Business</h3>
+                        <h4 class="hdg hdg_white">We Understand Business</h4>
                         <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
                     </li>
                     <li>
-                        <h3>We're small but get big results</h3>
+                        <h4 class="hdg hdg_white">We're small but get big results</h4>
                         <p>Being small means you'll work directly with talented information architects and usability experts who are passionate about what we do and invested in your success.</p>
                     </li>
                     <li>
-                        <h3>We cut out the unimportant stuff</h3>
+                        <h4 class="hdg hdg_white">We cut out the unimportant stuff</h4>
                         <p>We've learned a lot from working with Fortune 500 leaders, established brands, and start-ups in retail, consumer goods, financial services, hospitality, healthcare and technology.</p>
                     </li>
                 </ul>
