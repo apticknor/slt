4cda8ead52e6529e6c39a32c4b186c4035a24012
Anthony Ticknor <tony.ticknor@gmail.com>
2013-02-22 00:50:05 -0600

    complete rework of contact section based on new PSD and direction from zeus jones

--- includes/sections/contact.php
+++ includes/sections/contact.php
@@ -17,7 +17,6 @@
                                         <div class="adr">
                                             <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
                                             <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span><br/>
-                                            <a class="email" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
                                             <span class="tel">
                                                 <span class="type isHidden">work</span>
                                                 <span class="value">(612) 455-3388</span>
@@ -26,6 +25,8 @@
                                                 <span class="type isHidden">fax</span>
                                                 <span class="value">(612) 455-3389</span>
                                             </span>
+                                            <br/>
+                                            <a class="email link-highlight" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
                                         </div>
                                     </div>
                                 </div>
