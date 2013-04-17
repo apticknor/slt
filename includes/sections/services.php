0737b2f755fda038ed5fc9ac12cdad205a54a2fe
Dan Piscitiello <dpisciti@gmail.com>
2013-04-16 23:15:10 -0500

    Add equal heights logic to our difference section

--- includes/sections/services.php
+++ includes/sections/services.php
@@ -3,8 +3,8 @@
             <div class="section-hd section-hd_services">
                 <h2 class="hdg hdg_section hdg_green">We help companies simplify and improve their websites, applications and mobile experiences.</h2>
             </div>
-            <div class="section-bd section-bd_services">
-                <ul class="blocks blocks_3up" data-height-watch-group="servicesBoxes" data-height-watch-disable-width="656">
+            <div class="section-bd section-bd_services" data-height-watch-group="servicesBoxes" data-height-watch-disable-width="656">
+                <ul class="blocks blocks_3up" data-height-watch-group="servicesHeadlines" data-height-watch-disable-width="656">
                     <li>
                         <div class="box" data-height-watch-group-member="servicesBoxes">
                             <div class="feature">
@@ -14,7 +14,7 @@
                                     </div>
                                 </div>
                                 <div class="feature-hd js-heightWatch-item">
-                                    <h3 class="hdg_subSection hdg_green">Understand Your Users.</h3>
+                                    <h3 class="hdg_subSection hdg_green" data-height-watch-group-member="servicesHeadlines">Understand Your Users.</h3>
                                 </div>
                                 <div class="feature-bd">
                                     <p>Do you have a complete picture of the people you want to reach? Through user research and ethnographic studies, we'll help you gain a deeper understanding of your users' behaviors, motivations and goals.</p>
@@ -31,7 +31,7 @@
                                     </div>
                                 </div>
                                 <div class="feature-hd">
-                                    <h3 class="hdg_subSection hdg_green">Define the Experience.</h3>
+                                    <h3 class="hdg_subSection hdg_green" data-height-watch-group-member="servicesHeadlines">Define the Experience.</h3>
                                 </div>
                                 <div class="feature-bd">
                                     <p>We're experts at creating experiences that are easy to use. We'll work with you to plan and prototype the right solution. Our sitemaps, task models and wireframes capture all the details and help you visualize every interaction.</p>
@@ -48,7 +48,7 @@
                                     </div>
                                 </div>
                                 <div class="feature-hd">
-                                    <h3 class="hdg_subSection hdg_green">Test, Evaluate &amp; Improve.</h3>
+                                    <h3 class="hdg_subSection hdg_green" data-height-watch-group-member="servicesHeadlines">Test, Evaluate &amp; Improve.</h3>
                                 </div>
                                 <div class="feature-bd">
                                     <p>We'll test your site or product with real people to see how well it supports their needs. Then we'll provide straightforward solutions for fixing any problems. No long-winded reports, just the stuff you need to make it better.</p>
