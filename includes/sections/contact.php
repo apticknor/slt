6ae599291b6860a659dfbaa3179ba5d16554219e
Anthony Ticknor <tony.ticknor@gmail.com>
2013-05-12 16:16:41 -0500

    positioning map pin and overlay properly fo rall sizes of map

--- includes/sections/contact.php
+++ includes/sections/contact.php
@@ -2,42 +2,44 @@
         <div class="section section_contact" role="contentinfo" id="page-contact">
             <div class="section-bd section-bd_contact">
                 <div class="map">
-                    <a href="http://goo.gl/maps/zxYDZ" rel="external" class="map-pin"></a>
                     <div class="map-overlay">
-                        <div class="box box_mute box_basic box_withArrow">
-                            <div class="teaser">
-                                <div class="teaser-hd">
-                                    <h2 class="hdg hdg_subSection hdg_bold">
-                                        <span class="textlineMobile">Want to know more?</span>
-                                        <span class="textlineMobile">Contact Us.</span>
-                                    </h2>
-                                </div>
-                                <div class="teaser-subHd">
-                                    <h3 class="hdg hdg_minorSection hdg_bold hdg_cap hdg_lightGreen"><span class="fn org">Straight Line Theory</span></h3>
-                                </div>
-                                <div class="teaser-bd">
-                                    <div class="vcard">
-                                        <div class="adr">
-                                            <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
-                                            <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span>
-                                            <br/>
-                                            <span class="tel">
-                                                <span class="type hdg_lightGreen">Phone</span>
-                                                <span class="value">612.455.3388</span>
-                                            </span>
-                                            <br/>
-                                            <span class="tel">
-                                                <span class="type hdg_lightGreen">Fax</span>
-                                                <span class="value">612.455.3389</span>
-                                            </span>
-                                            <br/>
-                                            <span class="hdg_lightGreen">Email</span>
-                                            <a class="email link-highlight" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
+                        <a href="http://goo.gl/maps/zxYDZ" rel="external" class="map-overlay-pin"></a>
+                        <div class="map-overlay-box">
+                            <div class="box box_mute box_basic box_withArrow">
+                                <div class="teaser">
+                                    <div class="teaser-hd">
+                                        <h2 class="hdg hdg_subSection hdg_bold">
+                                            <span class="textlineMobile">Want to know more?</span>
+                                            <span class="textlineMobile">Contact Us.</span>
+                                        </h2>
+                                    </div>
+                                    <div class="teaser-subHd">
+                                        <h3 class="hdg hdg_minorSection hdg_bold hdg_cap hdg_lightGreen"><span class="fn org">Straight Line Theory</span></h3>
+                                    </div>
+                                    <div class="teaser-bd">
+                                        <div class="vcard">
+                                            <div class="adr">
+                                                <span class="street-address">510 First Avenue North</span>, <span class="extended-address">Suite 408</span><br/>
+                                                <span class="locality">Minneapolis</span>, <span class="region">MN</span> <span class="postal-code">55403</span>
+                                                <br/>
+                                                <span class="tel">
+                                                    <span class="type hdg_lightGreen">Phone</span>
+                                                    <span class="value">612.455.3388</span>
+                                                </span>
+                                                <br/>
+                                                <span class="tel">
+                                                    <span class="type hdg_lightGreen">Fax</span>
+                                                    <span class="value">612.455.3389</span>
+                                                </span>
+                                                <br/>
+                                                <span class="hdg_lightGreen">Email</span>
+                                                <a class="email link-highlight" href="mailto:info@straightlinetheory.com">info@straightlinetheory.com</a><br/>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="teaser-ft">
-                                    <a class="link-highlight" href="http://goo.gl/maps/zxYDZ">Get Directions</a>
+                                    <div class="teaser-ft">
+                                        <a class="link-highlight" href="http://goo.gl/maps/zxYDZ">Get Directions</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
