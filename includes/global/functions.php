c320b2d6a0b3b6715dd5fcacfa0beccb4dc4ddb4
Anthony Ticknor <tony.ticknor@gmail.com>
2013-07-09 00:37:23 -0500

    adding configurable minification to CSS

--- includes/global/functions.php
+++ includes/global/functions.php
@@ -1,6 +1,66 @@
 <?php
 
+    // ********************************
+    // Variables
+    // ********************************
+
     // boolen (true / false) to determine if orange box for hiring is visble
     $show_hiring_box = false;
+    $minify_enabled = true;
+
+    // ********************************
+    // CSS Minification
+    // ********************************
+    // Require the compressor
+    require($site_path . 'libraries/cssmin.php');
+    function minifyCSS($filename) {
+
+        global $site_url;
+        global $site_path;
+        global $minify_enabled;
+
+        if ($minify_enabled == true) {
+
+            // Extract the CSS code you want to compress from your CSS files
+            $inputCSS = file_get_contents($site_path . 'assets/styles/' . $filename);
+
+            // Create a new CSSmin object.
+            // By default CSSmin will try to raise PHP settings.
+            // If you don't want CSSmin to raise the PHP settings pass FALSE to
+            // the constructor i.e. $compressor = new CSSmin(false);
+            $compressor = new CSSmin();
+
+            // Override any PHP configuration options before calling run() (optional)
+            $compressor->set_memory_limit('256M');
+            $compressor->set_max_execution_time(120);
+
+            // Compress the CSS code in 1 long line and store the result in a variable
+            $outputCSS = $compressor->run($inputCSS);
+
+            // You can change any PHP configuration option between run() calls
+            // and those will be applied for that run
+            $compressor->set_pcre_backtrack_limit(3000000);
+            $compressor->set_pcre_recursion_limit(150000);
+
+            // add .min to the file name before .css
+            $fileNameBits = explode('.', $filename);
+            $originalSuffix = array_pop($fileNameBits);
+            array_push($fileNameBits, 'min');
+            array_push($fileNameBits, $originalSuffix);
+            $new_filename = implode('.', $fileNameBits);
+
+             // write output to file
+            file_put_contents($site_path . 'assets/styles/' . $new_filename, $outputCSS);
+
+            // define the minified file url
+            $url = $site_url . 'assets/styles/' . $new_filename;
+
+        } else {
+            // define the original url
+            $url = $site_url . 'assets/styles/' . $filename;
+        }
+        // return the proper url
+        echo($url);
+    }
 
 ?>
--- includes/global/head.php
+++ includes/global/head.php
@@ -11,7 +11,7 @@
         <!-- SEO -->
         <title>Straight Line Theory</title>
         <meta name="description" content="Straight Line Theory helps companies make their websites and online applications easier to use. We enable customers to evaluate and improve their sites by learning more about their users, resolving usability issues and creating intuitive experiences that deliver measurable results." />
-        <meta name="keywords" content="information architecture, IA, usability consulting, usability testing, usability expert, usability lab, web usability, user experience, user interface design, site architecture, wireframe, site map, scenarios, personas, Straight Line Theory, Straightline Theory, SLT, John Dusek, Andrew Benson, Jay Mathews, Roy Neal, Laura Horan, Kristen Skifstad" />
+        <meta name="keywords" content="information architecture, IA, usability consulting, usability testing, usability expert, usability lab, web usability, user experience, user interface design, site architecture, wireframe, site map, scenarios, personas, Straight Line Theory, Straightline Theory, SLT, John Dusek, Andrew Benson, Heather Lindholm, Jay Mathews, Roy Neal, Laura Horan" />
         <meta name="author" content="Straight Line Theory" />
         <meta name="robots" content="index,follow,archive" />
         <link rel="canonical" href="http://straightlinetheory.com/" />
@@ -24,25 +24,25 @@
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $site_url; ?>apple-touch-icon-144x144-precomposed.png" />
 
         <!-- STYLESHEETS -->
-        <link rel="stylesheet" media="screen and (min-width: 1em)" href="<?php echo $site_url; ?>assets/styles/screen.css" />
-        <link rel="stylesheet" media="screen and (min-width: 30em)" href="<?php echo $site_url; ?>assets/styles/screen-sm.css" /> <!-- 480px -->
-        <link rel="stylesheet" media="screen and (min-width: 42em)" href="<?php echo $site_url; ?>assets/styles/screen-md.css" /> <!-- 672px -->
-        <link rel="stylesheet" media="screen and (min-width: 60em)" href="<?php echo $site_url; ?>assets/styles/screen-lg.css" /> <!-- 960px -->
-        <link rel="stylesheet" media="screen and (min-width: 84em)" href="<?php echo $site_url; ?>assets/styles/screen-xl.css" /> <!-- 1344px -->
-        <link rel="stylesheet" media="print" href="<?php echo $site_url; ?>assets/styles/print.css" />
+        <link rel="stylesheet" media="screen and (min-width: 1em)" href="<?php minifyCSS('screen.css') ?>" />
+        <link rel="stylesheet" media="screen and (min-width: 30em)" href="<?php minifyCSS('screen-sm.css') ?>" /> <!-- 480px -->
+        <link rel="stylesheet" media="screen and (min-width: 42em)" href="<?php minifyCSS('screen-md.css') ?>" /> <!-- 672px -->
+        <link rel="stylesheet" media="screen and (min-width: 60em)" href="<?php minifyCSS('screen-lg.css') ?>" /> <!-- 960px -->
+        <link rel="stylesheet" media="screen and (min-width: 84em)" href="<?php minifyCSS('screen-xl.css') ?>" /> <!-- 1344px -->
+        <link rel="stylesheet" media="print" href="<?php minifyCSS('print.css') ?>" />
 
         <!-- IE STYLESHEETS SANS MEDIAQUERIES -->
         <!--[if IE 8]>
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen.css" />
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-sm.css" />
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-md.css" />
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-lg.css" />
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-xl.css" />
+        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen.min.css" />
+        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-sm.min.css" />
+        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-md.min.css" />
+        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-lg.min.css" />
+        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/screen-xl.min.css" />
         <![endif]-->
 
         <!-- LEGACY IE SUPPORT -->
         <!--[if lt IE 8]>
-        <link rel="stylesheet" media="screen" href="<?php echo $site_url; ?>assets/styles/ie-legacy.css" />
+        <link rel="stylesheet" media="screen" href="<?php minifyCSS('ie-legacy.css') ?>" />
         <![endif]-->
     </head>
     <body id="page-top">
